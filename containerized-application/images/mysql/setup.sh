c32b6823d52a08870e8095916082c154e52f0ba9
Silvio Fragnani <silviofragnanisilva@gmail.com>
2021-03-06 20:55:18 +0100

    Deleted containerized-application

--- containerized-application/images/mysql/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker pull mysql
-
-docker run --rm -d --name mysql -e MYSQL_ROOT_PASSWORD=changeme -e MYSQL_DATABASE=items -e MYSQL_USER=silvio -e MYSQL_PASSWORD=qwerty123 -p 30306:3306 -v ~/../../database/mysql/:/var/lib/mysql mysql
